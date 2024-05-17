90b3f428ecf65917c3b4cd6ada8f448d8a83a758
Lalomax <lalomax365@gmail.com>
2024-05-17 16:10:45 -0400

    Exercise 3.2: Phone book backend - getting persons info

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+// getting all persons
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
+// getting info
+app.get("/info", (request, response) => {
+  const entries = persons.length;
+  var utc = new Date();
+  var local = new Date(utc.getTime());
+  response.send(
+    `<p>Phonebook has info for ${entries} people</p><br><p>${local}</p>`
+  );
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
