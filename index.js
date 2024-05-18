1c6e1b7ee0dbe8596d52a6084f9cb95c777dc81c
Lalomax <lalomax365@gmail.com>
2024-05-18 18:11:44 -0400

    exercise 3.3 getting one person info

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ app.get("/info", (request, response) => {
   );
 });
 
+// getting one person
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
