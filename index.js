28d4460ffbedb5ab7beb5d664ce80269a03a10ee
Lalomax <lalomax365@gmail.com>
2024-05-21 18:40:08 -0400

    feat: adding cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json());
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
 let persons = [
   {
@@ -90,7 +92,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
