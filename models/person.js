b19a3236a18658359c086de97f117b8dd24532fa
Lalomax <lalomax365@gmail.com>
2024-07-11 17:59:13 -0400

    feat: 3.21 Phone Number validation

--- models/person.js
+++ models/person.js
@@ -19,7 +19,12 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: {type : String, minLength: 3},
-  number: String,
+  number: {type : String, minLength: 8, validate: {
+    validator: function(v) {
+      return /\d{2,3}-\d{5,}/.test(v)
+    },
+    message: props => `${props.value} is not a valid phone number!`
+  }, required: [true, 'User phone number required']},
 })
 
 personSchema.set('toJSON', {
